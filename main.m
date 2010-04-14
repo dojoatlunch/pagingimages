6d5c827f78cade9f4615328066227408b13e74d7
Van Charles Tran <vctran@pyxis-tech.com>
2010-04-14 15:07:26 +0200

    Adding files

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PagingImages
+//
+//  Created by Van Charles Tran on 14/04/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
